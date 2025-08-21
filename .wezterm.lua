3c84a384463be49c0cd4506763ac88e12545721a
Kosuke Nomoto <kosukenomoto@gmail.com>
2025-08-21 22:50:34 +0900

    some change

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,16 +1,28 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+config.audible_bell = "Disabled"
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+config.colors = {
+  visual_bell = '#202020',
+}
+
 config.font = wezterm.font 'UDEV Gothic NF'
 config.color_scheme = 'Ayu Dark (Gogh)'
-config.font_size = 14.0
+config.font_size = 12.0
 config.initial_rows = 40
-config.initial_cols = 120 
+config.initial_cols = 150
 
 -- 背景の非透過率（1なら完全に透過させない）
-config.window_background_opacity = 0.92
+config.window_background_opacity = 0.8
 -- 他にもオプションを組み合わせるときれいになるよ
 config.enable_wayland = true -- Linux Waylandなら有効にしておく
+config.enable_tab_bar= false
 
 config.window_decorations = 'RESIZE'
 config.window_padding = {
@@ -41,10 +53,15 @@ local function set_font_size(size)
   end)
 end
 
-local act = wezterm.action
+
+config.disable_default_key_bindings = true
+
 config.keys = {
+  -- コピー
+  { key = "c", mods = "SHIFT|CTRL", action = wezterm.action.CopyTo("Clipboard") },
+  -- 貼り付け
+  { key = "v", mods = "SHIFT|CTRL", action = wezterm.action.PasteFrom("Clipboard") },
   { key = '9', mods = 'ALT|CTRL', action = set_font_size(18) },
   { key = '0', mods = 'ALT|CTRL', action = set_font_size(14) },
 }
-
 return config
